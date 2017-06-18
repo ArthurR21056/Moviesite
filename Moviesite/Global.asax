cfd1996053e5ac8c517ef775f061caeb138608f6
arthu <arthu@DESKTOP-RDM2UBS>
2017-06-17 20:43:52 -0500

    Add project files.

---
+++ Moviesite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Moviesite.MvcApplication" Language="C#" %>
---
+++ Moviesite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Moviesite.Startup))]
+namespace Moviesite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
