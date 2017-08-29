02f2b5d610e90dde4bf927f1197939c072e19bb5
Arthur Reeves <arthurreevesiii@gmail.com>
2017-08-29 10:13:23 -0500

    Web Api and Auto mapping

---
+++ Moviesite/Controllers/API/CustomersController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Moviesite.Models;
+using Moviesite.DTOs;
+using AutoMapper;
+
+namespace Moviesite.Models.API
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        } 
+
+        // GET /api/customers
+        public IEnumerable<CustomerDTO> GetCustomers()
+        {
+            return _context.Customer.ToList().Select(Mapper.Map<Customer, CustomerDTO>);
+        }
+        
+        // GET /api/customer/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customer.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return BadRequest();
+
+            return Ok(Mapper.Map<Customer, CustomerDTO>(customer));
+        }
+        // Post /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDTO customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var customer = Mapper.Map<CustomerDTO, Customer>(customerDto);
+            _context.Customer.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
+        }
+
+        // PUT /api/customer/1
+        [HttpPut]
+        public void UpdateCustomer(int id, CustomerDTO customerDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customer.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map<CustomerDTO, Customer>(customerDto, customerInDb);
+
+            _context.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customer.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customer.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Moviesite/DTOs/CustomerDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Moviesite.Models;
+
+namespace Moviesite.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+       // [Min18YearsIfMember]
+        public DateTime Birthdate { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+    }
+}
